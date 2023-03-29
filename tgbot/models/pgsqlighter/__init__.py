eace910d19e347db22f03b73cd65b7110624817f
outp1 <outplayed2511@mail.ru>
2023-03-29 14:52:34 +0300

    modified pgsqlighter to orm with repositoryes

--- tgbot/models/pgsqlighter/__init__.py
+++
@@ -1,1 +0,0 @@
-from .sqlighter import DatabaseConnection
---
+++ tgbot/models/users.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+from sqlalchemy.orm import Session
+
+from .orm.base import Repository
+from .orm.users import UserModel
+
+UserID = int
+
+
+@dataclass
+class User:
+    id: UserID
+    username: str
+
+
+class UsersRepository(Repository):
+    def __init__(self, session: Session, identity_map=None):
+        self.session = session
+        self.repository_name = "users"
+        self._identity_map = identity_map or {self.repository_name: {}}
+        self.entity_class = User
+        self.model_class = UserModel
